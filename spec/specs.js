64d71024edac47cfce0c0d9572ff0cd287c0dfdc
Jordan Johansen & charlesamoss <student@epicodus.com>
2015-09-10 11:45:20 -0700

    add first test:passes

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('roman', function() {
   it("inputs a number and outputs a string", function() {
-    expect(roman(1999)).to.be.an('array');
+    expect(roman(1999)).to.be.a('string');
   });
 
   // it("is true for most years divisible by 4", function() {
