4bd12046e110e21b06300bd631a5c545f871a27d
Jordan Johansen & charlesamoss <student@epicodus.com>
2015-09-10 11:42:27 -0700

    add scaffold and first test:fails

---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+describe('roman', function() {
+  it("inputs a number and outputs a string", function() {
+    expect(roman(1999)).to.be.an('array');
+  });
+
+  // it("is true for most years divisible by 4", function() {
+    // expect(leapYear(2012)).to.equal(true);
+  // });
+});
